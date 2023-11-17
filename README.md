86ec6a6a564b503283b827438343b0917165e981
bitquant <bitquant@bitquant.com>
2023-11-16 21:35:53 -0600

    support Request arguments

--- README.md
+++ README.md
@@ -8,25 +8,25 @@ $ npm install fetchout
 
 ## Usage
 ```javascript
-const fetch = require('fetchout');
+const fetchout = require('fetchout');
 
 // Default timeout for all requests. If not set the default is 60 seconds.
-fetch.defaultTimeout(5000);
+fetchout.defaultTimeout(5000);
 
 
 (async function() {
 
     try {
         // default timeout of 5 seconds used
-        let response1 = await fetch('http://httpbin.org/delay/3');
+        let response1 = await fetchout('http://httpbin.org/delay/3');
         console.log(`response1 status ${response1.status}`)
 
         // Set a 7 sec timeout for a GET fetch request
-        let response2 = await fetch('http://httpbin.org/delay/3', 7000);
+        let response2 = await fetchout('http://httpbin.org/delay/3', 7000);
         console.log(`response2 status ${response2.status}`)
 
         // Set a 6 sec timeout for a POST fetch request - timeout error
-        let response3 = await fetch('http://httpbin.org/delay/9',
+        let response3 = await fetchout('http://httpbin.org/delay/9',
            { method: 'POST', body: '{"foo": "bar"}' }, 6000);
     }
     catch (err) {
--- index.js
+++ index.js
@@ -6,16 +6,34 @@
 
 var defaultTimeout = 60000;
 
-function fetchout(url, params = { }, timeout = defaultTimeout) {
+function fetchout(resource, options, timeout = defaultTimeout) {
 
-    if (typeof(params) === 'number') {
-        timeout = params;
-        params = { }
+    if (typeof(options) === 'number') {
+        timeout = options;
+        options = null;
+    }
+
+    if (options instanceof Request) {
+        // Request objects are read-only and not enumerable so extract to a new object to add signal property
+        // Duplex discussion
+        // https://fetch.spec.whatwg.org/#dom-requestinit-duplex
+        // https://github.com/nodejs/node/issues/46221
+        const { method, headers, body, mode, credentials, cache, redirect, referrer, referrerPolicy, integrity } = options;
+        options = { method, headers, body, mode, credentials, cache, redirect, referrer, referrerPolicy, integrity, duplex: 'half' };
+    }
+
+    if (options === undefined || options === null) {
+        if (resource instanceof Request) {
+            const { method, headers, body, mode, credentials, cache, redirect, referrer, referrerPolicy, integrity } = resource;
+            options = { method, headers, body, mode, credentials, cache, redirect, referrer, referrerPolicy, integrity };
+        } else {
+            options = { }
+        }
     }
 
     let controller = new AbortController();
-    params.signal = controller.signal;
-    let fetchPromise = fetch(url, params);
+    options.signal = controller.signal;
+    let fetchPromise = fetch(resource, options);
     let timeoutId = setTimeout(() => controller.abort(), timeout);
 
     let errorHandler = (err) => {
@@ -41,3 +59,29 @@ fetchout.defaultTimeout = function(timeout) {
 
 
 module.exports = fetchout;
+
+async function convertReadableStreamToUint8Array(readableStream) {
+
+    let reader = readableStream.getReader();
+    let streamDone = false;
+    let chunkList = [];
+    let bytesReceived = 0;
+
+    while (!streamDone) {
+        let streamResult = await reader.read();
+        if (!streamResult.done) {
+            chunkList.push(streamResult.value);
+            bytesReceived += streamResult.value.length;
+        } 
+        streamDone = streamResult.done;
+    }
+
+    let result = new Uint8Array(bytesReceived);
+    let offset = 0;
+    for (let chunk of chunkList) {
+        result.set(chunk, offset);
+        offset += chunk.length;
+    }
+
+    return result;
+}
